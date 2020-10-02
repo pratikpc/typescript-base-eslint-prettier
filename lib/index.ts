e27f39c9a535704a005992dd40e38b2a367abaf1
Pratik Chowdhury <pratikc@live.co.uk>
2020-10-03 02:05:35 +0530

    Added Template Code

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+// Replace this entire code
+// With something else
+
+async function main() {
+    return new Promise<number>((resolve) => resolve(4));
+}
+
+main()
+    .then((success) => success)
+    .catch((err) => console.error(err));
